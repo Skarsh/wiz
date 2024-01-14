aab9a8d4c0b96b5de7e23ea338dee350c3cc7e6a
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2024-01-14 14:10:35 +0100

    Add examples folder with opengl.zig file

---
+++ examples/opengl.zig
@@ -0,0 +1,44 @@
+const std = @import("std");
+
+const wiz = @import("wiz");
+
+const opengl32 = wiz.opengl32;
+const Event = wiz.Event;
+const KeyEvent = wiz.KeyEvent;
+const Window = wiz.Window;
+const WindowFormat = wiz.WindowFormat;
+
+pub fn main() !void {
+    std.debug.print("hello world!\n", .{});
+
+    var window = try Window.init(std.heap.page_allocator, 640, 480, WindowFormat.fullscreen, "opengl-example");
+    try window.makeOpenGLContext();
+    opengl32.glViewport(0, 0, window.width, window.height);
+
+    var event: Event = Event{ .KeyDown = KeyEvent{ .scancode = 0 } };
+    while (window.running) {
+        try Window.processMessages();
+        while (window.event_queue.poll(&event)) {
+            switch (event) {
+                .KeyDown => {
+                    if (event.KeyDown.scancode == 1) {
+                        window.windowShouldClose(true);
+                    }
+                    if (event.KeyDown.scancode == 33) {
+                        try window.toggleFullscreen();
+                    }
+                },
+                else => {
+                    std.debug.print("Event: {}\n", .{event});
+                },
+            }
+        }
+        opengl32.glClearColor(1.0, 0.0, 1.0, 0.0);
+        opengl32.glClear(opengl32.GL_COLOR_BUFFER_BIT);
+        try window.swapBuffers();
+
+        std.time.sleep(1_000_000);
+    }
+
+    std.debug.print("Exiting app\n", .{});
+}
