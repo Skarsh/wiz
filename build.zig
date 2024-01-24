9a2e44e65f760da6ad2651e50ad3dd615607a66d
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2024-01-24 22:24:03 +0100

    Add building for tracy

--- build.zig
+++ build.zig
@@ -3,12 +3,37 @@ const Build = std.Build;
 const ResolvedTarget = Build.ResolvedTarget;
 const Compile = Build.Step.Compile;
 
-fn buildOpengl(b: *Build, opengl_exe: *Compile) void {
+fn buildOpengl(b: *Build, opengl_exe: *Compile, target: ResolvedTarget, build_options_module: *std.Build.Module, enable_tracy: bool) void {
     const wiz_module = b.addModule("wiz", .{
         .root_source_file = .{ .path = "src/wiz.zig" },
     });
 
+    wiz_module.addImport("build_options", build_options_module);
+
     opengl_exe.root_module.addImport("wiz", wiz_module);
+
+    if (enable_tracy) {
+        const client_cpp = "src/tracy/public/TracyClient.cpp";
+
+        // On mingw, we need to opt into windows 7+ to get some features required by tracy.
+        const tracy_c_flags: []const []const u8 = if (target.result.isMinGW())
+            &[_][]const u8{ "-DTRACY_ENABLE=1", "-fno-sanitize=undefined", "-D_WIN32_WINNT=0x601" }
+        else
+            &[_][]const u8{ "-DTRACY_ENABLE=1", "-fno-sanitize=undefined" };
+
+        opengl_exe.addIncludePath(.{ .path = "src/tracy" });
+        opengl_exe.addCSourceFile(.{
+            .file = .{ .path = client_cpp },
+            .flags = tracy_c_flags,
+        });
+        opengl_exe.linkLibCpp();
+        opengl_exe.linkLibC();
+
+        if (target.result.os.tag == .windows) {
+            opengl_exe.linkSystemLibrary("dbghelp");
+            opengl_exe.linkSystemLibrary("ws2_32");
+        }
+    }
 
     b.installArtifact(opengl_exe);
 
@@ -21,6 +46,7 @@ pub fn build(
 ) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const enable_tracy = b.option(bool, "enable_tracy", "Whether tracy should be enabled.") orelse false;
 
     const lib = b.addStaticLibrary(.{
         .name = "wiz",
@@ -39,6 +65,39 @@ pub fn build(
         .target = target,
         .optimize = optimize,
     });
+
+    const exe_options = b.addOptions();
+    exe_options.addOption(bool, "enable_tracy", enable_tracy);
+    exe_options.addOption(bool, "enable_tracy_allocation", b.option(
+        bool,
+        "enable_tracy_allocation",
+        "Enable using TracyAllocator to monitor allocations.",
+    ) orelse enable_tracy);
+    exe_options.addOption(bool, "enable_tracy_callstack", b.option(bool, "enable_tracy_callstack", "Enable callstack graphs.") orelse enable_tracy);
+    exe.root_module.addImport("build_options", exe_options.createModule());
+
+    if (enable_tracy) {
+        const client_cpp = "src/tracy/public/TracyClient.cpp";
+
+        // On mingw, we need to opt into windows 7+ to get some features required by tracy.
+        const tracy_c_flags: []const []const u8 = if (target.result.isMinGW())
+            &[_][]const u8{ "-DTRACY_ENABLE=1", "-fno-sanitize=undefined", "-D_WIN32_WINNT=0x601" }
+        else
+            &[_][]const u8{ "-DTRACY_ENABLE=1", "-fno-sanitize=undefined" };
+
+        exe.addIncludePath(.{ .path = "src/tracy" });
+        exe.addCSourceFile(.{
+            .file = .{ .path = client_cpp },
+            .flags = tracy_c_flags,
+        });
+        exe.linkLibCpp();
+        exe.linkLibC();
+
+        if (target.result.os.tag == .windows) {
+            exe.linkSystemLibrary("dbghelp");
+            exe.linkSystemLibrary("ws2_32");
+        }
+    }
 
     exe.linkSystemLibrary("opengl32");
 
@@ -61,7 +120,7 @@ pub fn build(
         .target = target,
         .optimize = optimize,
     });
-    buildOpengl(b, opengl_example_exe);
+    buildOpengl(b, opengl_example_exe, target, exe_options.createModule(), enable_tracy);
 
     const opengl_example_run_cmd = b.addRunArtifact(opengl_example_exe);
     opengl_example_run_cmd.step.dependOn(b.getInstallStep());
