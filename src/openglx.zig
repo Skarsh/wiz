7cc4398164dc2d43b4a4231114b34198f7882fc8
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2024-06-06 19:40:36 +0200

    Add pub keyword for openglx functions

--- src/openglx.zig
+++ src/openglx.zig
@@ -1,14 +1,14 @@
 //pub var glClearColor: *const fn (f32, f32, f32, f32) callconv(.C) void = undefined;
 //pub var glClear: *const fn (c_uint) callconv(.C) void = fn a{}void;
 
-fn glClearColor(r: f32, g: f32, b: f32, a: f32) callconv(.C) void {
+pub fn glClearColor(r: f32, g: f32, b: f32, a: f32) callconv(.C) void {
     _ = r;
     _ = g;
     _ = b;
     _ = a;
 }
 
-fn glClear(val: c_uint) callconv(.C) void {
+pub fn glClear(val: c_uint) callconv(.C) void {
     _ = val;
 }
 
